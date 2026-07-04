b0110d6e1a05e31fbc49a7b62b74e9d15d0753dc
FranK12821 <franziska.kostler@graduateinstitute.ch>
2026-07-04 21:17:53 +0200

    Add sources field to historical data entry

--- data.js
+++ data.js
@@ -6,5 +6,6 @@ const anecdotePoints = [
       place: "Königsberg, Preußen",
       description: "Two preachers, Dr Johann Wilhelm Ebel and Georg Heinrich Diestel, are accused of forming a sect and of encouraging inappropriate sexual behaviour between the men and women of their circle, including several of the highest born women of Königsberg, such as Ida von der Gröben - sister in law to the presiding judge. Closer examination instead reveal themes of resistance against the impositions of the public-private, rational-emotional dualisms that would come to refigure relations between men and women decidedly to the latters detriment.",
       portraitUrl: "Img/Ansicht_Koenigsberg_um_1850.jpg",
-      borderColor: "#8B0000"}
+      borderColor: "#8B0000",
+      sources: "Christopher Clark (2021): Prisoners of Time, Chapter: From Prussia with Love: Zealotry, Liberalism and the Public Sphere in 1830s Königsberg"}
    ];
