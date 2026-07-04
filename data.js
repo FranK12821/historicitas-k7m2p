4a513198db925fa467fb3b57ca7c3304f873c290
FranK12821 <franziska.kostler@graduateinstitute.ch>
2026-07-04 19:24:30 +0200

    Fix date formatting in anecdotePoints array

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const anecdotePoints = [
      {lat: 54.44, 
       lng: 20.29, 
       name: "The Königsberg Religious or Mucker Trial",
-      date: "1835"
+      date: "1835",
       place: "Königsberg, Preußen",
       description: "Two preachers, Dr Johann Wilhelm Ebel and Georg Heinrich Diestel, are accused of forming a sect and of encouraging inappropriate sexual behaviour between the men and women of their circle, including several of the highest born women of Königsberg, such as Ida von der Gröben - sister in law to the presiding judge. Closer examination instead reveal themes of resistance against the impositions of the public-private, rational-emotional dualisms that would come to refigure relations between men and women decidedly to the latters detriment.",
       portraitUrl: "Img/Ansicht_Koenigsberg_um_1850.jpg",
