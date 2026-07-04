457af0400622dfc877634d5d58158de452787769
FranK12821 <franziska.kostler@graduateinstitute.ch>
2026-07-04 18:55:06 +0200

    Update portrait URL for Königsberg trial entry

--- data.js
+++ data.js
@@ -5,6 +5,6 @@ const anecdotePoints = [
       date: "1835"
       place: "Königsberg, Preußen",
       description: "Two preachers, Dr Johann Wilhelm Ebel and Georg Heinrich Diestel, are accused of forming a sect and of encouraging inappropriate sexual behaviour between the men and women of their circle, including several of the highest born women of Königsberg, such as Ida von der Gröben - sister in law to the presiding judge. Closer examination instead reveal themes of resistance against the impositions of the public-private, rational-emotional dualisms that would come to refigure relations between men and women decidedly to the latters detriment.",
-      portraitUrl: "img/portraits/person1.jpg",
+      portraitUrl: "Img/Ansicht_Koenigsberg_um_1850.jpg",
       borderColor: "#8B0000"}
    ];
